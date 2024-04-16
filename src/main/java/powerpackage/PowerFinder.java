a8493137cc3cd2fe817e21278822f85130cf6bff
Aks32216 <amishsingh568@gmail.com>
2024-04-16 13:38:06 +0530

    added multiply function to improve modularity

--- src/main/java/powerpackage/PowerFinder.java
+++ src/main/java/powerpackage/PowerFinder.java
@@ -4,8 +4,11 @@ public class PowerFinder {
     public static long OF(int base, int exponent) {
         int raisedPower=1;
         for (int i = 1; i <= exponent; i++) {
-            raisedPower *= base;
-        }
+            raisedPower = multiply(raisedPower, base);        }
         return raisedPower;
+    }
+
+    private static int multiply(int number1,int number2) {
+        return number1 * number2;
     }
 }
